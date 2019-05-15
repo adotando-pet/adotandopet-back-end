1cec59fbebc1745e4679940fc628b059befa4665
Phellipe Rodrigues <phellipephellipe@hotmail.com>
2019-05-14 21:25:41 -0300

    criando metodos do controller e relacionamentos do model Pets

--- app/Models/Pet.js
+++ app/Models/Pet.js
@@ -4,6 +4,15 @@
 const Model = use('Model')
 
 class Pet extends Model {
+    user () {
+        return this.belongsTo('App/Models/User')
+      }
+    category () {
+        return this.belongsTo('App/Models/Category')
+     }
+     files () {
+        return this.belongsToMany('App/Models/File').withTimestamps()
+      }
 }
 
 module.exports = Pet
